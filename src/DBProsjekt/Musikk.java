e8caeb275de0def14da4c359c675a103917b80a9
Kirsten-Elise Rensaa <kerensaa@dhcp-10-22-61-97.wlan.ntnu.no>
2020-03-05 11:48:29 +0100

    oops

--- src/DBProsjekt/Musikk.java
+++ src/DBProsjekt/Musikk.java
@@ -1,4 +1,14 @@
 package DBProsjekt;
 
 public class Musikk {
+
+    public static int KomponistID;
+    public static String KomponistNavn;
+    public static String ArtistNavn;
+
+    public Musikk(int KomponistID, String KomponistNavn, String ArtistNavn){
+        this.KomponistID = KomponistID;
+        this.KomponistNavn = KomponistNavn;
+        this.ArtistNavn = ArtistNavn;
+    }
 }
--- src/DBProsjekt/Sjanger.java
+++ src/DBProsjekt/Sjanger.java
@@ -1,4 +1,11 @@
 package DBProsjekt;
 
 public class Sjanger {
+    public static int sjangerID;
+    public static String navn;
+
+    public Sjanger(int sjangerID, String navn){
+        this.sjangerID  = sjangerID;
+        this.navn = navn;
+    }
 }
